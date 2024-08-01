90263bb0a8cb3d3908808a7187db355314dee1a9
mitchelljordan <mitchell.jordan@dcmail.ca>
2024-08-01 12:08:01 -0400

    added loop

--- assign4/part2/main.js
+++ assign4/part2/main.js
@@ -6,16 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
-const alts = {'pic1.jpg' : 'Eye','pic2.jpg' : 'Shell close up','pic3.jpg' : 'Flowers','pic4.jpg' : 'Tomb wall','pic5.jpg' : 'Moth'
-  }
 
 /* Declaring the alternative text for each image file */
+const alts = {'pic1.jpg' : 'Eye','pic2.jpg' : 'Shell close up','pic3.jpg' : 'Flowers','pic4.jpg' : 'Tomb wall','pic5.jpg' : 'Moth'}
 
 /* Looping through images */
-
+for (image of images) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
+newImage.setAttribute('src', 'images/${image}');
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
-
+}
 /* Wiring up the Darken/Lighten button */
