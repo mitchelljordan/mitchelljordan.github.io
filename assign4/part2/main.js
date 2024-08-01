2919fe90861fbf5efdddedbd8074ae87ab4d000c
mitchelljordan <mitchell.jordan@dcmail.ca>
2024-08-01 11:46:28 -0400

    part 1 done and start of part 2
    added images array but still no images

---
+++ assign4/part2/main.js
@@ -0,0 +1,21 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {'pic1.jpg' : 'Eye','pic2.jpg' : 'Shell close up','pic3.jpg' : 'Flowers','pic4.jpg' : 'Tomb wall','pic5.jpg' : 'Moth'
+  }
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
