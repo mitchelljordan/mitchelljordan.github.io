fc253924a4a206df90a672b90765cd3b828ff07c
mitchelljordan <mitchell.jordan@dcmail.ca>
2024-08-01 13:43:16 -0400

    test one
    have been copying the code and just ran test 1 to put my ball  on the screen

--- assign4/part3/bouncingballsmain.js
+++ assign4/part3/bouncingballsmain.js
@@ -1,4 +1,4 @@
-// setup canvas
+// set up canvas
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -12,7 +12,7 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
+// function to generate random RGB color value
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
@@ -28,11 +28,10 @@ class Ball {
     this.size = size;
   }
 
-draw() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
