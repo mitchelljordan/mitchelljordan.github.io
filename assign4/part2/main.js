600c75c201307e4933bdba5a0148c812ec9d3ccf
mitchelljordan <mitchell.jordan@dcmail.ca>
2024-08-01 12:26:34 -0400

    images are displayed

--- assign4/part2/main.js
+++ assign4/part2/main.js
@@ -8,13 +8,17 @@ const overlay = document.querySelector('.overlay');
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
-const alts = {'pic1.jpg' : 'Eye','pic2.jpg' : 'Shell close up','pic3.jpg' : 'Flowers','pic4.jpg' : 'Tomb wall','pic5.jpg' : 'Moth'}
+const alts = {'pic1.jpg' : 'Eye', 'pic2.jpg' : 'Shell close up', 'pic3.jpg' : 'Flowers', 'pic4.jpg' : 'Tomb wall', 'pic5.jpg' : 'Moth'}
 
 /* Looping through images */
 for (image of images) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', 'images/${image}');
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', `images/${image}`);
+newImage.setAttribute('alt', alts[image]);
 thumbBar.appendChild(newImage);
 }
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', e => {
+
+})
+
