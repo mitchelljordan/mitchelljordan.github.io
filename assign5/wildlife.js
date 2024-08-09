5820af02e3c403c98e6f69a99831f6249379f91b
mitchelljordan <mitchell.jordan@dcmail.ca>
2024-08-08 22:59:08 -0400

    added tabbing feature

--- assign5/wildlife.js
+++ assign5/wildlife.js
@@ -1,20 +1,31 @@
 // functionality for showing/hiding the comments section
 
+// functionality for showing/hiding the comments section
+
 const showHideBtn = document.querySelector('.show-hide');
 const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
-showHideBtn.onclick = function() {
+function toggleComments() {
   let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
+  if (showHideText === 'Show comments') {
     showHideBtn.textContent = 'Hide comments';
     commentWrapper.style.display = 'block';
   } else {
     showHideBtn.textContent = 'Show comments';
     commentWrapper.style.display = 'none';
   }
+}
+
+showHideBtn.onclick = toggleComments;
+
+showHideBtn.onkeydown = function(e) {
+  if (e.key === 'Enter') {
+    toggleComments();
+  }
 };
+
 
 // functionality for adding a new comment via the comments form
 
